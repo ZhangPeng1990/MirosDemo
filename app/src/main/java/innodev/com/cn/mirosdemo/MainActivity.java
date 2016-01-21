9ea6ba947793f99e977d32290ea94511ee9c6538
zhangpeng <ZP151189@163.com>
2016-01-21 18:19:48 +0800

    0121

--- app/src/main/java/innodev/com/cn/mirosdemo/MainActivity.java
+++ app/src/main/java/innodev/com/cn/mirosdemo/MainActivity.java
@@ -3,6 +3,8 @@ package innodev.com.cn.mirosdemo;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -11,7 +13,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.RelativeLayout;
+import android.widget.VideoView;
+
+import java.util.List;
 
 public class MainActivity extends Activity{ //AppCompatActivity {
 
@@ -23,7 +29,7 @@ public class MainActivity extends Activity{ //AppCompatActivity {
     float lastX, lastY;
 
     ImageView iv2;
-    WebView wv;
+    VideoView vv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +40,21 @@ public class MainActivity extends Activity{ //AppCompatActivity {
         //属性动画
         this.setContentView(R.layout.activity_main);
 
+        init();
+    }
+
+    protected void init(){
+
         rl = (RelativeLayout) findViewById(R.id.r1);
 
         iv = (ImageView) findViewById(R.id.iv);
         iv2 = (ImageView) findViewById(R.id.iv2);
+
+        vv = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("http://v.youku.com/v_show/id_XMTQ1MTAyODE2MA==.html");
+        vv.setMediaController(new MediaController(this));
+        vv.setVideoURI(uri);
+        vv.requestFocus();
 
         iv.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -112,6 +129,49 @@ public class MainActivity extends Activity{ //AppCompatActivity {
                         // 属性动画移动
                         ObjectAnimator y = ObjectAnimator.ofFloat(iv2, "y", iv2.getY(), nextY);
                         ObjectAnimator x = ObjectAnimator.ofFloat(iv2, "x", iv2.getX(), nextX);
+
+                        AnimatorSet animatorSet = new AnimatorSet();
+                        animatorSet.playTogether(x, y);
+                        animatorSet.setDuration(0);
+                        animatorSet.start();
+
+                        lastX = event.getRawX();
+                        lastY = event.getRawY();
+                }
+                return false;
+            }
+        });
+
+        vv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        lastX = event.getRawX();
+                        lastY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //  不要直接用getX和getY,这两个获取的数据已经是经过处理的,容易出现图片抖动的情况
+                        float distanceX = lastX - event.getRawX();
+                        float distanceY = lastY - event.getRawY();
+
+                        float nextY = vv.getY() - distanceY;
+                        float nextX = vv.getX() - distanceX;
+
+                        // 不能移出屏幕
+                        if (nextY < 0) {
+                            nextY = 0;
+                        } else if (nextY > containerHeight - vv.getHeight()) {
+                            nextY = containerHeight - vv.getHeight();
+                        }
+                        if (nextX < 0)
+                            nextX = 0;
+                        else if (nextX > containerWidth - vv.getWidth())
+                            nextX = containerWidth - vv.getWidth();
+
+                        // 属性动画移动
+                        ObjectAnimator y = ObjectAnimator.ofFloat(vv, "y", vv.getY(), nextY);
+                        ObjectAnimator x = ObjectAnimator.ofFloat(vv, "x", vv.getX(), nextX);
 
                         AnimatorSet animatorSet = new AnimatorSet();
                         animatorSet.playTogether(x, y);
